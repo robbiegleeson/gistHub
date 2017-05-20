9b76396d817600ef9bbabdcf23cd8d365d9d7c55
Andrew Winterbotham <awinterbotham02@gmail.com>
2017-05-20 23:40:53 +0000

    add ability to delete gist from selection and simplify by calling yield on promises wrapped in co

--- index.js
+++ index.js
@@ -12,7 +12,6 @@ const opn = require('opn');
 const request = require('request');
 const commandLineArgs = require('command-line-args');
 const colors = require('colors');
-const Enquirer = require('enquirer');
 
 const read = thunkify(fs.readFile);
 const DEFAULT_DESCRIPTION = 'Created with gistHub | by Rob Gleeson';
@@ -58,84 +57,84 @@ function *validateArgs(options) {
 }
 
 function *showUserGists(username) {
-    var password = yield requestUserPassword();
-    var gitRequest = request.defaults({
-        headers: {
-            'User-Agent': 'ua'
-        },
-        auth: {
-            'username': username,
-            'password': password
-        }
-    });
+    co(function* () {
+        try {
+            var password = yield requestUserPassword();
+            var gitRequest = request.defaults({
+                headers: {
+                    'User-Agent': 'ua'
+                },
+                auth: {
+                    'username': username,
+                    'password': password
+                }
+            });
 
-    gitRequest(usersUrl + username + '/gists', function(err, resp, body) {
-        if (err) {
+            gitRequest = thunkify(gitRequest);
+            
+            var resp = (yield gitRequest(usersUrl + username + '/gists'))[0];
+            if (resp.statusCode === 403 || resp.statusCode === 401) {
+                console.log(`Error: ${resp.statusMessage}`.red);
+                process.exit(0);
+            }
+
+            var obj = JSON.parse(resp.body);
+            console.log('got here');
+            var gists = Object.keys(obj).map(function (key) {
+                return obj[key];
+            });
+
+            var newObj = {};
+            console.log('File\t\t\t\t\tCreated Time\t\t\tID'.grey);
+            for (var i = 0; i < gists.length; i++) {
+                var gist = gists[i];
+                console.log((i + 1) + ' ' +  Object.keys(gist.files)+'\t\t\t'+gist.created_at+'\t\t'+gist.id);
+            }
+
+            if (!gists.length) {
+                process.exit(0);
+            }
+
+            var answer = yield prompt('Do you wish to delete any of the above gists: y/n '.yellow);
+            if (answer.toLowerCase() === 'y' || answer.toLowerCase() === 'yes') {
+                var nextAnswer = yield prompt('Please enter the number from the table above that you wish to delete '.yellow);
+                if (!isNaN(parseInt(nextAnswer)) && parseInt(nextAnswer) <= gists.length && parseInt(nextAnswer) >=  1) {
+                    yield deleteGist(gists[parseInt(nextAnswer) - 1].id, username);
+                } else {
+                    console.log('The number you entered was not in the table above, please try again');
+                }
+            } else {
+                console.log('Those gists are just too good to delete!');
+            }
+        } catch (err) {
             console.log(`Error: ${err}`.red);
-            process.exit(0);
         }
 
-        if (resp.statusCode === 403 || resp.statusCode === 401) {
-            console.log(`Error: ${resp.statusMessage}`.red);
-            process.exit(0);
-        }
-
-        var obj = JSON.parse(resp.body);
-        var gists = Object.keys(obj).map(function (key) {
-            return obj[key];
-        });
-
-       
-    
-        var newObj = {};
-        console.log('File\t\t\t\t\tCreated Time\t\t\tID'.grey);
-        for (var i = 0; i < gists.length; i++) {
-            var gist = gists[i];
-            console.log((i + 1) + ' ' +  Object.keys(gist.files)+'\t\t\t'+gist.created_at+'\t\t'+gist.id);
-        }
-
-       var enquirer = new Enquirer();
-       enquirer.register('confirm', require('prompt-confirm'));
-
-       enquirer.question('delete', 'Do you wish to delete any of the above gists?', {type: 'confirm'});
-       enquirer.prompt('delete')
-       .then(function(answer) {
-           if (!answer.delete) {
-               enquirer.question('selection', 'Please enter the number from the list you wish to delete: ');
-               
-               enquirer.ask('selection')
-               .then(function(answer) {
-                   if (!isNaN(parseInt(answer.selection)) && parseInt(answer.selection) >= gists.length) {
-                       console.log(answer.selection);
-                   }
-                })
-                .catch(function(err) {
-                    console.log(err)
-                });
-           }
-        });
+        process.exit(0);
     });
 }
 
 function createGist(args, title, fileContent) {
     co(function *() {
-        const password = yield requestUserPassword();
-        var client = new GitHub({
-           username: args.username,
-           password: password
-        });
+        try {
+            const password = yield requestUserPassword();
+            var client = new GitHub({
+               username: args.username,
+               password: password,
+            });
 
-        var gist = client.getGist();
+            var gist = client.getGist();
 
-        gist.create({
-            public: args.isPublic,
-            description: args.description,
-            files: {
-                [title]: {
-                    content: fileContent,
+            var resp = yield gist.create({
+                public: args.isPublic,
+                description: args.description,
+                files: {
+                    [title]: {
+                        content: fileContent,
+                    }
                 }
-            }
-        }).then(function (resp) {
+            });
+        
             if (resp.status === 201) {
                 clipboard.copy(resp.data.html_url, () => {
                     co(function *() {
@@ -145,17 +144,18 @@ function createGist(args, title, fileContent) {
                             opn(resp.data.html_url);
                         } else {
                             console.log(`Copied Gist URL to clipboard!`.green);
-                        }
-                        process.exit(0)
+                         }
+                         process.exit(0);
                     });
                 });
             } else if (!resp) {
                 console.log('No response'.red);
-                process.exit(0)
+                process.exit(0);
             }
-        }).catch(function (err) {
-            console.log('Error: Unale to authenticate you.'.red);
-        });
+        } catch (err) {
+            console.log('Error: could not authenticate you');
+            process.exit(0);
+        }    
     });
 }
 
@@ -173,30 +173,23 @@ function *deleteGist(gistId, username) {
         });
 
         const confirm = yield prompt('Are you sure you want to delete that Gist?: y/n '.yellow);
-
+        
+        gitRequest.delete = thunkify(gitRequest.delete);
         if (confirm.toLowerCase() === 'y' || confirm.toLowerCase() === 'yes') {
-            gitRequest.delete(gistUrl + gistId, function(err, resp, body) {
-                if (err) {
-                    console.log(`Error: ${err}`.red);
-                    process.exit(0);
-                }
-
-                if (resp.statusCode === 403 ||
-                    resp.statusCode === 401 ||
-                    resp.statusCode === 404) {
-                    console.log(`Error: ${resp.statusMessage}`.red);
-                    process.exit(0);
-                } else {
-                    console.log('Gist was deleted!'.green);
-                    process.exit(0);
-                }
-            });
-        } else {
-            process.exit(0);
+            var resp = (yield gitRequest.delete(gistUrl + gistId))[0];
+            
+            if (resp.statusCode === 403 ||
+                resp.statusCode === 401 ||
+                resp.statusCode === 404) {
+                console.log(`Error: ${resp.statusMessage}`.red);
+            } else {
+                console.log('Gist was deleted!'.green);
+            }
         }
     } catch(err) {
         console.log('Error: ' + err);
     }
+    process.exit(0);
 }
 
 commander
